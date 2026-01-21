03e11283a00e35ac197dd113d859710537c63d96
lcdse7en <2353442022@qq.com>
2026-01-21 16:05:40 +0000

    add

--- code/fcitx5.sh
+++
@@ -1,3 +0,0 @@
-sudo pacman -S fcitx5-im fcitx5-rime fcitx5-chinese-addons
-sudo pacman -S fcitx5-hangul fcitx5-anthy
-sudo pacman -S fcitx5-material-color
--- code/vpn.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/xishang0128/sparkle/releases/download/1.6.16/sparkle-linux-1.6.16-x64.pkg.tar.xz
-tar -xvf sparkle-linux-1.6.16-x64.pkg.tar.xz
-# (copy sparkle directory to ~/.local/bin)
